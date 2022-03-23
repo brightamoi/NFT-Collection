37ba985c607f2800964ac5508a0a49281da8f770
brightamoi <amoi.bright92@gmail.com>
2022-03-23 11:00:32 -0400

    contract deployed

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xBdba8D63e3fb86Cae750F2F1dbe14263298E617D";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
